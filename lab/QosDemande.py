b06c59890f4914d1c44d4239aecc17f372f0da30
mona-quimamba <claudio.antonio@etu.unilim.fr>
2022-01-11 21:16:48 +0100

    ajouter truc

--- lab/QosDemande.py
+++ lab/QosDemande.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 import subprocess
 
-interface_reseau = b"wlo2"
+interface_reseau = b"r1-eth0"
 filtre_ecoute = b"'ip and (ip[1] & 0x03 == 0x03)'"
 ecoute_reseau = subprocess.Popen(b"tcpdump -c 1 -ln -i %s %s"%(interface_reseau,filtre_ecoute),shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 print("Resultat du TCPDUMP :" ,ecoute_reseau.stdout.read())
